7199e6ece04f6dd5c4443fd14c1a49e24956a614
shinkkhs <158053656+shinkkhs@users.noreply.github.com>
2026-01-19 16:29:26 +0900

    Update about page cycle diagram

--- src/components/Icons.tsx
+++ src/components/Icons.tsx
@@ -33,8 +33,26 @@ export const Icons = {
       strokeLinejoin="round"
       {...props}
     >
-      <path d="M2 3h6a4 4 0 0 1 4 4v14a3 3 0 0 0-3-3H2z" />
-      <path d="M22 3h-6a4 4 0 0 0-4 4v14a3 3 0 0 1 3-3h7z" />
+      <path d="M3 7a2 2 0 0 1 2-2h5l2 2h7a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z" />
+    </svg>
+  ),
+  Library: (props: IconProps) => (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      {...props}
+    >
+      <path d="M3 5a2 2 0 0 1 2-2h6v16H5a2 2 0 0 1-2-2z" />
+      <path d="M21 5a2 2 0 0 0-2-2h-6v16h6a2 2 0 0 0 2-2z" />
+      <path d="M7 7h2" />
+      <path d="M7 11h2" />
     </svg>
   ),
   Signals: (props: IconProps) => (
